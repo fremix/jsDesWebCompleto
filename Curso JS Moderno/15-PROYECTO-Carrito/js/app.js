5c40a00d2347f5fb897ca6d36456e54fd82d5266
fremix <microsys.net@gmail.com>
2022-04-27 02:46:48 -0500

    carrito 2

--- Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
+++ Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -15,10 +16,31 @@ function cargarEventListeners() {
 function agregarCurso(e) { // para prevenir el eventboubling
     e.preventDefault(); // Previene la accion por defualt
 
-    if (e.target.classList.contains('agregar-carrito')) {// .classList para poder ver las clases de lo que le estamos dando click
-        console.log(e.target); 
+    if( e.target.classList.contains('agregar-carrito') ) {
+        // .classList para poder ver las clases de lo que le estamos dando click
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+
+    }       
 }
+    
+/** Lee el contenido del HTML al que le dimos click y extrae la informacion del curso */
+function leerDatosCurso( curso ) {
+    // console.log(curso);
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent, // .textContent es para extrare el contenido de un texto
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'), // Para obtener el ID: .getAttribute('data-id')
+        cantidad: 1
+
     }
     
-/** Lee la informacion del HTML al que le dimos click y extrae la informacion del curso */
+    // Agrega elementos al arreglo del carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
 
+    console.log(articulosCarrito);
+
+
+}
