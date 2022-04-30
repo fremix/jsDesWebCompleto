b106b77b115c199967093c7a32c2bd3b8cf625d4
fremix <microsys.net@gmail.com>
2022-04-29 22:44:24 -0500

    expresion regular

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -29,19 +29,29 @@ function iniciarApp() {
 
 // Valida el formulario
 function validarFormulario(e) {
+
+    
     
     if(e.target.value.length > 0) {
         console.log('Si hay algo...');
     } else {
         e.target.classList.add('border', 'border-red-500');
 
-        mostrarError();
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if(e.target.type === 'email') { // Validacion del @ en el email
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        if(resultado < 0) {
+            mostrarError('El email es invalido');
+        }
     }
 }
 
-function mostrarError() {
+function mostrarError( mensaje ) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error'); // Muestra solo un mensaje de error
